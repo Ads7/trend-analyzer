62fd0fa2ded677db553b67c9f56a299cf2fa0afe
Ads7 <ads71993@gmail.com>
2015-04-10 21:26:10 +0530

    test1

--- static/js/compareview.js
+++ static/js/compareview.js
@@ -3,42 +3,55 @@ $(function(){
 table = $('table');
 
    $.getJSON('./temp.json', function(data) {
-    $.each(data, function(i, d) {
-    output = "";
-    output += "<tr>";    
+  
+      
     for (j=0;j<4;j++){
-
-    	output += "<td>";
-    		output += d[j];
+        output = "";
+        output += "<tr>"; 
+    	for (i=0;i<3;i++){
+        output += "<td>";
+    		output += data[i][j];
     		output += "</td>";
     	}
     	output += "</tr>";
     	table.children("tbody").append(output);
-    	console.log(d);
-    });
-    
-
-	});
-table.children("tbody").each(function() {
-        var $this = $(this);
-        var newrows = [];
-
-        $this.find("tr").each(function(){
-            var i = 0;
-
-            $(this).find("td").each(function(){
-                i++;
-                if(newrows[i] === undefined) { newrows[i] = $(""); }
-                newrows[i].append($(this));
-            });
-        });
-
-        $this.find("tr").remove();
-        $.each(newrows, function(){
-            $this.append(this);
-        });
+    }
     });
 
+var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+        var lineChartData = {
+            labels : ["January","February","March","April","May","June","July"],
+            datasets : [
+                {
+                    label: "My First dataset",
+                    fillColor : "rgba(220,220,220,0.2)",
+                    strokeColor : "rgba(220,220,220,1)",
+                    pointColor : "rgba(220,220,220,1)",
+                    pointStrokeColor : "#fff",
+                    pointHighlightFill : "#fff",
+                    pointHighlightStroke : "rgba(220,220,220,1)",
+                    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor : "rgba(151,187,205,0.2)",
+                    strokeColor : "rgba(151,187,205,1)",
+                    pointColor : "rgba(151,187,205,1)",
+                    pointStrokeColor : "#fff",
+                    pointHighlightFill : "#fff",
+                    pointHighlightStroke : "rgba(151,187,205,1)",
+                    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+                }
+            ]
+
+        }
+
+    window.onload = function(){
+        var ctx = document.getElementById("canvas").getContext("2d");
+        window.myLine = new Chart(ctx).Line(lineChartData, {
+            responsive: true
+        });
+    }
 
 });
 
